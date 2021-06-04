0b7e306af2753cf3a313be0c0b7a1877a27c415a
Wei Jian <2508469t@student.gla.ac.uk>
2021-06-04 13:52:29 +0800

    update

--- calculatorserver.java
+++ calculatorserver.java
@@ -16,18 +16,22 @@ public class calculatorserver {
    public calculatorserver() {
 
 		 try {
+			 System.setProperty("java.rmi.server.hostname", "52.149.157.162");
 			 System.out.println("Init server...\n");
-                         calculator c = new calculatorimpl();
+             calculator c = new calculatorimpl();
 		 	//Construct a new CalculatorImpl object and bind it to the local rmiregistry
      		//N.b. it is possible to host multiple objects on a server
 			 
 			 System.out.println("Reg RMI...\n");
-                         LocateRegistry.createRegistry(port);
+             //LocateRegistry.createRegistry(port);  // For starting rmiregistry
+			 reg = LocateRegistry.createRegistry(port); 
+			 
 			 //Registry reg = LocateRegistry.createRegistry(port);
 			 // calculator c = new calculatorimpl(); // Instantiate the servant class into an object
 			 
-			 //reg.bind("CalculatorService", c);
-			 Naming.rebind("rmi://10.0.0.6/CalculatorService", c); // Binding it to the naming service, the name will be used to call its method
+			 reg.bind("CalculatorService", c);
+			 //Naming.rebind("rmi://10.0.0.6/CalculatorService", c); // Binding it to the naming service, the name will be used to call its method
+			 Naming.bind("10.0.0.6", c);
 			 //UnicastRemoteObject.exportObject(c, 1100);
 			 System.out.println("Reg RMI completed!\n");
 			 
@@ -41,7 +45,7 @@ public class calculatorserver {
 
    public static void main(String args[]) {
 	   
-	    System.setProperty("java.rmi.server.hostname", "52.149.157.162");
+	    //System.setProperty("java.rmi.server.hostname", "52.149.157.162");
 	    //System.setProperty("java.rmi.server.hostname", "10.0.0.6");
      	//Create the new Calculator server
 			if (args.length == 1)
